c8ea5471b6d4fa99e3a84811011edaf3832cf6fd
tinhi3 <lancetnguyen100@gmail.com>
2022-06-20 14:32:09 -0400

    starting phase

---
+++ index.js
@@ -0,0 +1,25 @@
+//Define SUITS/VALUES in the global scope
+const suits = ["♠", "♣", "♥", "♦"]
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K"]
+
+// Define the deck of cards
+class Deck {
+    constructor(cards) {
+        this.cards = []
+    }
+}
+
+// Define deck for individual card
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+// Function for fresh deck of cards
+
+const freshDeck = () => {
+    return suits.
+}
+
