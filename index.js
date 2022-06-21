56391a807dcb50bed8e5c172752cea5b1b7e1030
tinhi3 <lancetnguyen100@gmail.com>
2022-06-21 11:04:42 -0400

    creating more deck/card functions

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-//Define SUITS/VALUES in the global scope
-const suits = ["♠", "♣", "♥", "♦"]
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K"]
+// Define SUITS/VALUES in the global scope
+// Reference back later for suits and values
+// Will only use one time so use all CAPS for recognition
+// 2=2, 3=3, 4=4, 5=5, 6=6, 7=7, 8=8, 
+// 9=9 10=10, J=11, Q=12, K=13, A=14
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K"]
 
+// Deck functions for game usage
+// Maybe not the best way to organize functions?
 // Define the deck of cards
+// Pass the cards into the constructor parameter
+// Pass in freshDeck to show the cards and suits
+// Make sure to call freshDeck using () or the array will not show up!!
 class Deck {
-    constructor(cards) {
-        this.cards = []
+    constructor(cards = freshDeck()) {
+        this.cards = cards
     }
 }
 
+// 
+
+
 // Define deck for individual card
+// Suit and value for each card
 class Card {
     constructor(suit, value) {
         this.suit = suit
@@ -18,8 +31,59 @@ class Card {
 }
 
 // Function for fresh deck of cards
-
+// Map will bring back arrays within arrays
+// Use flatMap instead of map to condense the array of cards(MDN source)
+// flatMap used on VALUES brings back the same result
 const freshDeck = () => {
-    return suits.
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
+
+// Create a function that will help with not having to rewrite the index card legnth each time
+const numberofCards = () => {
+    return this.cards.length
+}
+
+// Shuffle function to randomize  cards
+// Math.floor(Math.random()) will return random integer between 0 & 1
+const shuffleCards = () => {
+    for (let i = this.numberOfCards -1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * (i + 1))
+        const oldValue = this.cards[newIndex]
+        this.cards[newIndex] = this.cards[i]
+        this.cards[i] = oldValue
+        console.log(shuffleCards)
+    }
+}
+
+const deck = new Deck()
+console.log(deck.cards)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+restart.addEventListener("click", function (event) {
+    location.reload()
+    console.log("reload page!")
+})
 
